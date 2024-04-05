61a51bae99b4a30ad3c382524b2650b20c76de55
Oksana Kaledina <kaledinaoa@gmail.com>
2024-04-05 16:21:05 +0300

    upd pre-commit

--- example.py
+++ example.py
@@ -3,7 +3,20 @@ import os
 import pandas as pd
 
 
-def test(a, b, c, d): ...
+def test(a: int, b: float, c: str, d: pd.DataFrame) -> int:
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (float): _description_
+        c (str): _description_
+        d (pd.DataFrame): _description_
+
+    Returns:
+        int: _description_
+
+    """
+    return 0
 
 
 os.abort
